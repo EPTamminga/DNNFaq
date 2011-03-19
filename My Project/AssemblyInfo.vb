d03a458ce40c5a83591462e0e4227ef7ebdc27a1
EPT_cp <SND\EPT_cp>
2011-03-19 06:10:21 -0700

    Updated the copyright notice in the source files to the current standard

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -17,7 +17,6 @@
 ' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 ' DEALINGS IN THE SOFTWARE.
 '
-Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
